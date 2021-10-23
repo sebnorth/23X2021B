0e1af9ea0d5a1404ef166076a6e3e2b1aba093ad
sebnorth <sebnorth@gmail.com>
2021-10-23 16:06:03 +0200

    Dodaj pliki projektów.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    /// <summary>
+    /// Exercise 2 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Uses ordered generic dynamic array
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            Prostokat prostokat1 = new Prostokat(3, 4);
+            Prostokat prostokat2 = new Prostokat(2, 5);
+
+            OrderedDynamicArray<Prostokat> tablica = new OrderedDynamicArray<Prostokat>();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Exercise2/Prostokat.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Prostokat : IComparable
+    {
+        int szerokosc;
+        int wysokosc;
+        //private int v1;
+        //private int v2;
+
+        public Prostokat(int szerokosc, int wysokosc)
+        {
+            this.szerokosc = szerokosc;
+            this.wysokosc = wysokosc;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Prostokat otherProstokat = obj as Prostokat;
+            if (otherProstokat != null)
+            {
+
+                // jeśli pole Prostokat < pole otherProstokat zwróć -1
+                // pole Prostokat = this.szerokosc*this.wysokosc
+                // jeśli pole Prostokat > pole otherProstokat zwróć 1
+                // jeśli pole Prostokat ==  pole otherProstokat zwróć 0
+                int poleProstokat = this.szerokosc*this.wysokosc;
+                int poleOtherProstokat = otherProstokat.szerokosc*otherProstokat.wysokosc;
+
+                if (poleProstokat < poleOtherProstokat)
+                {
+                    return -1;
+                }
+
+                if (poleOtherProstokat < poleProstokat)
+                {
+                    return 1;
+                }
+
+                else {
+
+                    return 0;
+                }
+
+            }
+          
+            else
+                throw new ArgumentException("Object is not a Prostokat");
+        }
+
+    }
+}
